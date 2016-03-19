0643a20ed6c2d4824f37d9f27d4e4e98cdf587b6
Shawn Allen <shawn.allen@gsa.gov>
2016-03-18 18:28:49 -0700

    implement depth layer, accessor, labels

--- js/components/climate-map.js
+++ js/components/climate-map.js
@@ -2,9 +2,9 @@
 
   var SUBDOMAINS = 'a b c d'.split(' ');
   var TILE_LAYERS = {
-    'toner-lite': 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
-    'toner': 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
-    'terrain': 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png'
+    background: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png',
+    labels: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}.png',
+    slr: 'https://{s}.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_{depth}ft/MapServer/tile/{z}/{y}/{x}'
   };
 
   var parseLatLng = function(str) {
@@ -116,7 +116,22 @@
           Object.keys(TILE_LAYERS)[0];
 
         var tileUrl = TILE_LAYERS[layerName] || layerName;
-        var tiles = L.tileLayer(tileUrl, {subdomains: SUBDOMAINS})
+        var tiles = L.tileLayer(tileUrl, {
+            subdomains: SUBDOMAINS
+          })
+          .addTo(map);
+
+        var depth = +this.getAttribute('depth') || 0;
+        this.xtag.depthLayer = L.tileLayer(TILE_LAYERS.slr, {
+            depth: depth,
+            subdomains: ['www', 'maps', 'maps1', 'maps2']
+          })
+          .addTo(map);
+
+        L.tileLayer(TILE_LAYERS.labels, {
+            subdomains: SUBDOMAINS,
+            opacity: .8
+          })
           .addTo(map);
 
         this.xtag.map = map;
@@ -126,6 +141,7 @@
         switch (name) {
           case 'center':
           case 'zoom':
+          case 'depth':
             this[name] = value;
             break;
         }
@@ -171,6 +187,23 @@
             [bbox[1], bbox[0]],
             [bbox[3], bbox[2]]
           ]);
+        }
+      },
+
+      depth: {
+        get: function() {
+          return this.xtag.depthLayer.options.depth;
+        },
+        set: function(depth) {
+          depth = +depth;
+          if (isNaN(depth)) {
+            return false;
+          }
+          if (depth !== this.depth) {
+            var layer = this.xtag.depthLayer;
+            layer.options.depth = depth;
+            layer.redraw();
+          }
         }
       }
     },
