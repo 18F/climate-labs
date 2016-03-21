d6a85e3d00f617bb7f6af6062bb685fd0c4a963b
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-03-21 12:08:09 -0400

    Use global query string fn

--- js/views/landing.js
+++ js/views/landing.js
@@ -1,20 +1,6 @@
 (function(exports){
-  exports.parseURLParameters = function (qs) {
-    var params = qs.replace(/^\?/g, '').split('&');
-    var ps = {};
-
-    params.forEach(function(p) {
-      var parts;
-      if (p) {
-        parts = p.split('=');
-        ps[parts[0]] = parts[1];
-      }
-    });
-
-    return ps;
-  };
-
-  var params = parseURLParameters(window.location.search);
+  var query = window.climate.parseQueryString;
+  var params = query(window.location.search);
   var formTest = params ? params.test : false;
 
   var formEl = document.querySelector('#query-form');
