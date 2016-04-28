8fe4f7b67f97026d3f5a898c5080ef704da08725
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-04-28 16:13:31 -0400

    Use DOM APIs for matching classes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 (function(window) {
-  function hasClass(el, c) {
-    var classes = el.className.split(' ');
-    var match;
-    if (classes.length === 0) return false;
-    match = classes.find(function(i) { return i === c; });
-    if (!match) return false;
-    return true;
-  }
-
   document.body.addEventListener('click', function(e) {
     if (e.target.matches('aside h3 span')) {
       var downloadEl = document.querySelector('#downloads');
-      if (hasClass(downloadEl, 'open')) {
+      if (downloadEl.matches('.open')) {
         return downloadEl.classList.remove('open');
       }
       return downloadEl.classList.add('open');
